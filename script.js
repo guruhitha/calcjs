e25835f253f29a612ec6f8438327d9fd323a7b78
guruhitha <dsandun73@gmail.com>
2025-08-18 18:22:55 +0530

    First

---
+++ script.js
@@ -0,0 +1,13 @@
+const dis = document.getElementById("display");
+    
+function appendDisplay(input) {
+    dis.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function calculator(){
+    
+}
